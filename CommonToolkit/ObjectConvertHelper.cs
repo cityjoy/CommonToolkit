0e1d6690b21516e1174291352940048c601cf814
jerry <llj.joy@163.com>
2016-07-06 11:03:31 +0800

    添加对象拷贝方法CopyOjbect

--- CommonToolkit/ObjectConvertHelper.cs
+++ CommonToolkit/ObjectConvertHelper.cs
@@ -12,6 +12,76 @@ namespace CommonToolKit
     public static class ObjectConvertHelper
     {
         /// <summary>
+        /// 对象拷贝
+        /// </summary>
+        /// <param name="obj">被复制对象</param>
+        /// <returns>新对象</returns>
+        public static object CopyOjbect(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            Object targetDeepCopyObj;
+            Type targetType = obj.GetType();
+            //值类型  
+            if (targetType.IsValueType == true)
+            {
+                targetDeepCopyObj = obj;
+            }
+            //引用类型   
+            else
+            {
+                targetDeepCopyObj = System.Activator.CreateInstance(targetType);   //创建引用对象   
+                System.Reflection.MemberInfo[] memberCollection = obj.GetType().GetMembers();
+
+                foreach (System.Reflection.MemberInfo member in memberCollection)
+                {
+                    //拷贝字段
+                    if (member.MemberType == System.Reflection.MemberTypes.Field)
+                    {
+                        System.Reflection.FieldInfo field = (System.Reflection.FieldInfo)member;
+                        Object fieldValue = field.GetValue(obj);
+                        if (fieldValue is ICloneable)
+                        {
+                            field.SetValue(targetDeepCopyObj, (fieldValue as ICloneable).Clone());
+                        }
+                        else
+                        {
+                            field.SetValue(targetDeepCopyObj, CopyOjbect(fieldValue));
+                        }
+
+                    }//拷贝属性
+                    else if (member.MemberType == System.Reflection.MemberTypes.Property)
+                    {
+                        System.Reflection.PropertyInfo myProperty = (System.Reflection.PropertyInfo)member;
+
+                        MethodInfo info = myProperty.GetSetMethod(false);
+                        if (info != null)
+                        {
+                            try
+                            {
+                                object propertyValue = myProperty.GetValue(obj, null);
+                                if (propertyValue is ICloneable)
+                                {
+                                    myProperty.SetValue(targetDeepCopyObj, (propertyValue as ICloneable).Clone(), null);
+                                }
+                                else
+                                {
+                                    myProperty.SetValue(targetDeepCopyObj, CopyOjbect(propertyValue), null);
+                                }
+                            }
+                            catch (System.Exception ex)
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+            return targetDeepCopyObj;
+        }
+        /// <summary>
         /// 使用此方法：
         /// var list = ObjectConverter.ConvertTo<实体类>(使用Linq查询出来的结果);
         /// </summary>
